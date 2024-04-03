7b83f92dc959c2db4b3e0245c8048b53768be397
ThoDev_Potter <93704520+haupotter72@users.noreply.github.com>
2024-04-03 15:16:27 +0700

    Update LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -24,7 +24,7 @@ function LoadMore() {
       });
     }
   }, [inView, data]);
-
+  
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
